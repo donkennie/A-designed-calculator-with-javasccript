02ca1ca326a4cd136d2366c8a97b17fdaf469b93
donkennie <ajeigbekehinde160@gmail.com>
2022-03-28 17:58:37 +0100

    feat: first commit

---
+++ calc.js
@@ -0,0 +1,34 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+    equal.addEventListener("click", function (e) {
+      if (screen.value === "") {
+        screen.value = "Please enter a number";
+      } else {
+        let answer = eval(screen.value);
+        screen.value = answer;
+     }
+   });
+
+  //   buttons.forEach((button) => {
+  //     if (button.id !== "equal") {
+  //       button.addEventListener("click", (e) => {
+  //         equal.value += e.target.id;
+  //       });
+  //     }
+  //   });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+})();
